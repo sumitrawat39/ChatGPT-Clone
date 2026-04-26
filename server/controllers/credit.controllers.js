8f000d60f872bf963cd34738850712498b3c14d7
sumit rawat <nirnay777@gmail.com>
2026-04-26 09:27:07 +0530

    adding credit controller

---
+++ server/controllers/credit.controllers.js
@@ -0,0 +1,73 @@
+import { use } from "react";
+import Transaction from "../models/transaction.model.js";
+
+const plans = [
+  {
+    _id: "basic",
+    name: "Basic",
+    price: 10,
+    credits: 100,
+    features: [
+      "100 text generations",
+      "50 image generations",
+      "Standard support",
+      "Access to basic models",
+    ],
+  },
+  {
+    _id: "pro",
+    name: "Pro",
+    price: 20,
+    credits: 500,
+    features: [
+      "500 text generations",
+      "200 image generations",
+      "Priority support",
+      "Access to pro models",
+      "Faster response time",
+    ],
+  },
+  {
+    _id: "premium",
+    name: "Premium",
+    price: 30,
+    credits: 1000,
+    features: [
+      "1000 text generations",
+      "500 image generations",
+      "24/7 VIP support",
+      "Access to premium models",
+      "Dedicated account manager",
+    ],
+  },
+];
+
+export const getPlans = async (req, res) => {
+  try {
+    res.json({ success: true, plans });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const purchasePlans = async (req, res) => {
+  try {
+    const { planId } = req.body;
+    const userId = req.user._id;
+    const plan = plan.find((plan) => plan._id === planId);
+
+    if (!plan) {
+      return res.json({ success: false, message: "Inavalid Plan" });
+    }
+
+    const transaction = await Transaction.create({
+      userId: userId,
+      planId: plan._id,
+      amount: plan.price,
+      credits: plan.credits,
+      isPaid: false,
+    });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
